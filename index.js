e4809f694e8f453fbb64e9d1e7af4ab006b8550f
Mehmet Köse <mehmetkosedev@gmail.com>
2021-02-03 02:13:17 +0300

    initial commit

---
+++ index.js
@@ -0,0 +1,33 @@
+const path = require("path");
+const fs = require("fs");
+const webfontsGenerator = require("webfonts-generator");
+
+const directoryPath = path.join(__dirname, "icons");
+
+let svgFiles = [];
+
+const files = fs.readdirSync(directoryPath);
+for (const file of files) {
+  const fileDir = path.join(directoryPath, file);
+  const location = `${path.dirname(fileDir)}/${path.basename(fileDir)}`;
+  svgFiles.push(location);
+}
+
+webfontsGenerator(
+  {
+    files: svgFiles,
+    dest: "dist/",
+    fontName: "remote-icon",
+    templateOptions: {
+      classPrefix: "remote-icon",
+      baseSelector: ".remote-icon",
+    },
+  },
+  function (error) {
+    if (error) {
+      console.log("Fail!", error);
+    } else {
+      console.log("Done!");
+    }
+  }
+);
